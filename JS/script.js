1b8e8c2961010baa15e3e84916fcc97ec9d3033f
ed-matheus <edsonmatheus02@hotmail.com>
2023-09-16 20:54:33 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,46 @@
+// JSON File Path
+const jsonFile = '../data.json'
+
+// HTML Elements (categories)
+const reactionElement = document.querySelector('#reaction .category')
+const memoryElement = document.querySelector('#memory .category')
+const verbalElement = document.querySelector('#verbal .category')
+const visualElement = document.querySelector('#visual .category')
+
+const reactionScore = document.querySelector('#reaction .mainScore')
+const memoryScore = document.querySelector('#memory .mainScore')
+const verbalScore = document.querySelector('#verbal .mainScore')
+const visualScore = document.querySelector('#visual .mainScore')
+
+const reactionIcon = document.querySelector('#reaction img')
+const memoryIcon = document.querySelector('#memory img')
+const verbalIcon = document.querySelector('#verbal img')
+const visualIcon = document.querySelector('#visual img')
+
+// Getting the JSON file
+fetch(jsonFile)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+        // Category
+        reactionElement.innerHTML = data[0].category;
+        memoryElement.innerHTML = data[1].category;
+        verbalElement.innerHTML = data[2].category;
+        visualElement.innerHTML = data[3].category;
+
+        // Score
+        reactionScore.innerHTML = data[0].score;
+        memoryScore.innerHTML = data[1].score;
+        verbalScore.innerHTML = data[2].score;
+        visualScore.innerHTML = data[3].score;
+
+        // Icons
+        reactionIcon.setAttribute("src", data[0].icon)
+        memoryIcon.setAttribute("src", data[1].icon)
+        verbalIcon.setAttribute("src", data[2].icon)
+        visualIcon.setAttribute("src", data[3].icon)
+    })
+    .catch(error => {
+        console.error('Erro ao carregar o arquivo JSON: ', error)
+    })
