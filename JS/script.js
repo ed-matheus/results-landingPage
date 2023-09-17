c34ff304dda307cbf39d748341eb6d60a07d3d1d
ed-matheus <edsonmatheus02@hotmail.com>
2023-09-16 21:36:49 -0300

    alteração geral nos arquivos

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,43 @@
-const jsonFile="../data.json",reactionElement=document.querySelector("#reaction .category"),memoryElement=document.querySelector("#memory .category"),verbalElement=document.querySelector("#verbal .category"),visualElement=document.querySelector("#visual .category"),reactionScore=document.querySelector("#reaction .mainScore"),memoryScore=document.querySelector("#memory .mainScore"),verbalScore=document.querySelector("#verbal .mainScore"),visualScore=document.querySelector("#visual .mainScore"),reactionIcon=document.querySelector("#reaction img"),memoryIcon=document.querySelector("#memory img"),verbalIcon=document.querySelector("#verbal img"),visualIcon=document.querySelector("#visual img");fetch("../data.json").then(a=>a.json()).then(a=>{console.log(a),reactionElement.innerHTML=a[0].category,memoryElement.innerHTML=a[1].category,verbalElement.innerHTML=a[2].category,visualElement.innerHTML=a[3].category,reactionScore.innerHTML=a[0].score,memoryScore.innerHTML=a[1].score,verbalScore.innerHTML=a[2].score,visualScore.innerHTML=a[3].score,reactionIcon.setAttribute("src",a[0].icon),memoryIcon.setAttribute("src",a[1].icon),verbalIcon.setAttribute("src",a[2].icon),visualIcon.setAttribute("src",a[3].icon)}).catch(a=>{console.error("Erro ao carregar o arquivo JSON: ",a)});
+const jsonFile = "../data.json"
+
+const reactionElement = document.querySelector("#reaction .category")
+const memoryElement = document.querySelector("#memory .category")
+const verbalElement = document.querySelector("#verbal .category")
+const visualElement = document.querySelector("#visual .category")
+
+const reactionScore = document.querySelector("#reaction .mainScore")
+const memoryScore = document.querySelector("#memory .mainScore")
+const verbalScore = document.querySelector("#verbal .mainScore")
+const visualScore = document.querySelector("#visual .mainScore")
+
+const reactionIcon = document.querySelector("#reaction img")
+const memoryIcon = document.querySelector("#memory img")
+const verbalIcon = document.querySelector("#verbal img")
+const visualIcon = document.querySelector("#visual img")
+
+fetch(jsonFile)
+    .then(response => response.json())
+    .then(data => {
+        console.log(a)
+
+        // Categories
+        reactionElement.innerHTML = data[0].category
+        memoryElement.innerHTML = data[1].category
+        verbalElement.innerHTML = data[2].category
+        visualElement.innerHTML = a[3].category
+
+        // Scores
+        reactionScore.innerHTML = data[0].score
+        memoryScore.innerHTML = data[1].score
+        verbalScore.innerHTML = data[2].score
+        visualScore.innerHTML = data[3].score
+
+        // Icons
+        reactionIcon.setAttribute("src", data[0].icon)
+        memoryIcon.setAttribute("src", data[1].icon)
+        verbalIcon.setAttribute("src", data[2].icon)
+        visualIcon.setAttribute("src", data[3].icon)
+    }).
+    catch(a => {
+        console.error("Erro ao carregar o arquivo JSON: ", a)
+    });
